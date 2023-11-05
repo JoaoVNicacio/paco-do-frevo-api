9da661904e025ead0bf2c59897e1c2ca57e597a9
Hernande <80050052+Nande14@users.noreply.github.com>
2023-11-05 02:42:05 +0000

    criação do service, dto e mapper de event

--- src/domain/services/ievent.service.ts
+++ src/domain/services/ievent.service.ts
@@ -1,9 +1,10 @@
+import EventDTO from 'src/application/dtos/associationDtos/event.dto';
 import Event from '../entities/associationAggregate/event.entity';
 
 interface IEventService {
-  createEvent(event: Event): Promise<Event>;
+  createEvent(eventDto: EventDTO): Promise<Event>;
   findById(id: string): Promise<Event | undefined>;
-  updateEvent(id: string, event: Event): Promise<Event | undefined>;
+  updateEvent(id: string, eventDto: EventDTO): Promise<Event | undefined>;
   deleteEvent(id: string): Promise<void>;
 }
 
