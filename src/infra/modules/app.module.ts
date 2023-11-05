9d7c0765f34ee80c47c67d514256b446edfafd3e
Hernande <80050052+Nande14@users.noreply.github.com>
2023-11-05 04:14:06 +0000

    Cria e ajusta módulos

--- src/infra/modules/app.module.ts
+++ src/infra/modules/app.module.ts
@@ -1,3 +1,4 @@
+import { EventModule } from './event.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
@@ -5,22 +6,36 @@ import AssociationAddress from 'src/domain/entities/associationAggregate/address
 import Association from 'src/domain/entities/associationAggregate/association.entity';
 import { AssociationModule } from './association.module';
 import Event from 'src/domain/entities/associationAggregate/event.entity';
+import { PhoneNumberModule } from './phoneNumber.module';
+import PhoneNumber from 'src/domain/entities/associationAggregate/phoneNumber.entity';
+import Contact from 'src/domain/entities/associationAggregate/contact.entity';
+import Member from 'src/domain/entities/associationAggregate/member.entity';
 
 dotenv.config();
 
 @Module({
   imports: [
+    EventModule,
+    PhoneNumberModule,
+    AssociationModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DB_URL,
       synchronize: true,
       logging: true,
       autoLoadEntities: true,
-      entities: [Association, AssociationAddress, Event],
+      entities: [
+        Association,
+        AssociationAddress,
+        Event,
+        Member,
+        PhoneNumber,
+        Contact,
+        Event,
+      ],
       migrations: ['src/migration/**/*.ts'],
       subscribers: ['src/subscriber/**/*.ts'],
     }),
-    AssociationModule,
   ],
   controllers: [],
   providers: [],
--- src/infra/modules/association.module.ts
+++ src/infra/modules/association.module.ts
@@ -6,19 +6,26 @@ import AssociationService from 'src/application/useCases/services/association.se
 import AssociationController from '../controllers/association.controller';
 import AssociationRepository from '../repositories/association.repository';
 import AssociationMapper from 'src/application/mappers/association.mapper';
-import AddressMapper from 'src/application/mappers/address.mapper';
 import Event from 'src/domain/entities/associationAggregate/event.entity';
 import Member from 'src/domain/entities/associationAggregate/member.entity';
 import PhoneNumber from 'src/domain/entities/associationAggregate/phoneNumber.entity';
+import Contact from 'src/domain/entities/associationAggregate/contact.entity';
+import { EventModule } from './event.module';
+import { PhoneNumberModule } from './phoneNumber.module';
+import AddressMapper from 'src/application/mappers/address.mapper';
 
 @Module({
   imports: [
+    EventModule,
+    PhoneNumberModule,
     TypeOrmModule.forFeature([
       Association,
       AssociationAddress,
       Event,
       Member,
       PhoneNumber,
+      Contact,
+      Event,
     ]),
   ],
   controllers: [AssociationController],
